262073cd3920d8ed8c5bdea8008ef3e5ab102c02
Norbert Frank Mba <norbertmbafrank@gmail.com>
2023-03-28 14:44:40 +0100

    simple deposit withdraw contract, with a simple re-entrancy vulnerability

---
+++ contracts/reentrancy attacks/FundStore.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.6;
+
+contract FundStore{
+    mapping (address => uint256) public balances;
+
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value; 
+    }
+
+    function withdraw(uint256 _amount) public payable {
+
+        require(balances[msg.sender] > _amount, "Insufficient fund");
+        (bool sent, ) = msg.sender.call{value: _amount}(""); // send funds to sender, Note: this line is vulnerable to a call back hence reentrancy
+        require(sent, "Failed to send funds");
+
+        balances[msg.sender] -= _amount;
+
+    }
+}
